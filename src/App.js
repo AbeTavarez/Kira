71edf02373e07b1da2329ad5708914bcf5bb0ce2
Abraham E. Tavarez <abetavarez@icloud.com>
2022-02-19 18:16:43 -0500

    mapping logs

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import { useEffect } from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import SearchBar from './components/SearchBar';
-
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 const App = () => {
   useEffect(() => {
     // inits MaterializeJS
     M.AutoInit();
-  })
+  });
   return (
-    <div className="App">
-     <h1>Hello</h1>
+    <>
       <SearchBar />
-    </div>
+      <Logs />
+    </>
   );
-}
+};
 
 export default App;
