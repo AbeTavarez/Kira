355f74dad962f06095eb1d972f0e89a7f049f6ac
Abraham E. Tavarez <abetavarez@icloud.com>
2022-02-19 18:33:10 -0500

    logs

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import LogsItem from './LogsItem';
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -19,13 +20,13 @@ const Logs = () => {
   if (loading) return <h4>Loading...</h4>;
 
   return (
-    <ul className="collection-with-header">
+    <ul className="collection with-header">
       <li className="collection-header">
         <h4 className="center">Logs</h4>
         {!loading && logs.length === 0 ? (
           <p className="center">No logs to show</p>
         ) : (
-          logs.map((log) => <li>{log.message}</li>)
+          logs.map((log) => <LogsItem log={log} key={log.id} />)
         )}
       </li>
     </ul>
