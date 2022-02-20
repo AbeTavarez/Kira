9a2a7ce5c0e0cf0fde229a2f3abd4b879387d6e6
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-02-19 20:16:46 -0500

    add button component to add new log or tech

---
+++ src/components/layout/AddButton.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AddButton = () => {
+  return (
+    <div className="fixed-action-btn">
+      <a className='btn-floating btn-large blue darken-2 modal-trigger' href="#add-log-modal">
+          <i className="large material-icons">add</i>
+      </a>
+      <ul>
+          <li className='btn-floating green modal-trigger' href='#tech-list-modal'>
+              <i className='material-icons'>person</i>
+          </li>
+          <li className='btn-floating red modal-trigger' href='#tech-modal'>
+              <i className='material-icons'>person_add</i>
+          </li>
+      </ul>
+    </div>
+  )
+}
+
+export default AddButton
