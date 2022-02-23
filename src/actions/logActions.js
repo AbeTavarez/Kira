7f5a19b8fa8bd0dbf6c05c8e153a22fe7aed8230
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-02-22 22:24:59 -0500

    new add log action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
 
 export const getLogs = () => async (dispatch) => {
   try {
@@ -17,7 +17,44 @@ export const getLogs = () => async (dispatch) => {
     });
   }
 };
-//* =============== first version
+
+export const addLog = (log) => async dispatch => {
+    try {
+        setLoading()
+        const data = await axios.post('/logs', log);
+
+        dispatch({
+            type: ADD_LOG,
+            payload: data
+        })
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        })
+    }
+}
+
+//* set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+//* =============== first version of get logs
 // export const getLogs = () => {
 //     return async (dispatch) => {
 //         setLoading();// set loading to true in the state
@@ -29,9 +66,3 @@ export const getLogs = () => async (dispatch) => {
 //         })
 //     }
 // }
-// set loading to true
-export const setLoading = () => {
-  return {
-    type: SET_LOADING,
-  };
-};
