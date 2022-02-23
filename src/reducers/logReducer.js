9d9ee5d8a3dc16bedd3b591fe795eeec5dbb69b1
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-02-22 21:10:09 -0500

    getlogs, logs erros, and set loading

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,8 +1,5 @@
 import {
-  ADD_TODO,
   GET_TODOS,
-  UPDATE_TODO,
-  DELETE_TODO,
   SET_LOADING,
   LOGS_ERROR,
   GET_LOGS,
@@ -13,29 +10,10 @@ const initialState = {
   current: null,
   loading: false,
   error: null,
-  toDos: [
-    {
-      userId: 1,
-      id: 1,
-      title: "delectus aut autem",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 2,
-      title: "quis ut nam facilis et officia qui",
-      completed: false,
-    },
-  ],
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case GET_TODOS:
-      return {
-        ...state,
-      };
-
     case GET_LOGS:
       return {
         ...state,
