09768248967def58e48bc966df75fab620da8f89
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-02-22 21:08:17 -0500

    data fetching fixed

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,35 +1,4 @@
-import { ADD_TODO, GET_TODOS, UPDATE_TODO, DELETE_TODO } from "./types";
 import axios from "axios";
-// export const getToDos = () => {
-//     // Redux Thunk allows us to return a function:
-//     return async () => {
-//         // first we fetch our data
-//         const data = await axios.get('https://jsonplaceholder.typicode.com/todos');
-//         // then we dispatch an action with a type and a payload which is the new data.
-//         dispatch({
-//             type: GET_TODOS,
-//             payload: data
-//         })
-//     }
-// }
-export const getToDos = () => async (dispatch) => {
-  // Redux Thunk allows us to return a function:
-  try {
-    // first we fetch our data
-    const data = await axios.get("https://jsonplaceholder.typicode.com/todos");
-    // then we dispatch an action with a type and a payload which is the new data.
-    dispatch({
-      type: GET_TODOS,
-      payload: data,
-    });
-  } catch (err) {
-      dispatch({
-          type: TODO_ERROR,
-          payload: err.response.data
-      })
-  }
-};
-
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 
 export const getLogs = () => async (dispatch) => {
@@ -39,7 +8,7 @@ export const getLogs = () => async (dispatch) => {
 
     dispatch({
       type: GET_LOGS,
-      payload: data,
+      payload: data.data,
     });
   } catch (err) {
     dispatch({
@@ -48,7 +17,7 @@ export const getLogs = () => async (dispatch) => {
     });
   }
 };
-//* first version
+//* =============== first version
 // export const getLogs = () => {
 //     return async (dispatch) => {
 //         setLoading();// set loading to true in the state
