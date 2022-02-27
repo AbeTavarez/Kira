6a7f78474fc191d98605b57898cfcd17d5f83af9
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-02-26 22:27:04 -0500

    Notfound page created

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (  
+    <div>
+      <h5>Oop.. something went wrong, this page doesn't exist.</h5>
+    </div>
+  )
+}
+
+export default NotFound
